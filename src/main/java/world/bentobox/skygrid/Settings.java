70a08dac5c267a82e65fdd8f2f2d3990aa7f1117
tastybento <tastybento@wasteofplastic.com>
2019-02-12 18:31:22 -0800

    Fixes system crash on new island
    https://github.com/BentoBoxWorld/SkyGrid/issues/10

--- src/main/java/world/bentobox/skygrid/Settings.java
+++ src/main/java/world/bentobox/skygrid/Settings.java
@@ -276,7 +276,7 @@ public class Settings implements DataObject, WorldSettings {
      */
     @Override
     public boolean isUseOwnGenerator() {
-        return false;
+        return true;
     }
 
     /**
