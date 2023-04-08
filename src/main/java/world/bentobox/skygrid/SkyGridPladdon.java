5d3b657ae3311a334664fef74c5f71e24c5f2443
BONNe <bonne@bonne.id.lv>
2023-04-08 19:14:48 +0300

    Update SkyGridPladdon.java

--- src/main/java/world/bentobox/skygrid/SkyGridPladdon.java
+++ src/main/java/world/bentobox/skygrid/SkyGridPladdon.java
@@ -1,13 +1,10 @@
 package world.bentobox.skygrid;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-@Plugin(name="SkyGrid", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_16)
+
 public class SkyGridPladdon extends Pladdon {
 
     @Override
